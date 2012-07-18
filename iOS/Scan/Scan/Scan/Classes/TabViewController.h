664c68d3cf3518e3d6205a33acf0e7ddb57dec76
Michael Critz <github@micahelcritz.com>
2012-07-18 09:24:35 -0700

    Cleanup iOS project

---
+++ iOS/Scan/Scan/Scan/Classes/TabViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabViewController.h
+//  Scan
+//
+//  Created by Michael Critz on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabViewController : UIViewController
+
+@end
---
+++ iOS/TabReader/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  TabReader
+//
+//  Created by Michael Critz on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UIViewController
+
+@end
